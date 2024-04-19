b214ca8983c3b0ee5bd93bd9ffe1ff09e1cbc7af
FraaQuartu <francescoquartu50@gmail.com>
2024-04-19 19:57:54 +0200

    Display digits when pressed

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ const operate = function(operator, num1, num2) {
       return 'Error';
   } 
 }
+
+
+let digit_keys = document.querySelectorAll(".digit");
+let display = document.querySelector("#display");
+
+// Faccio in modo di visualizzare i numeri che vengono digitati
+digit_keys.forEach(key => {
+  // If is clicked then display it
+  key.addEventListener("click", (e) => {
+    let digit = e.target.textContent;
+    if (digit == '.') {
+      // Se il separatore decimale è già nel display non aggiungere, altrimenti si
+      if (!display.textContent.includes('.')) {
+        display.textContent += digit;
+      }
+    }
+    else {
+      // Non voglio che i numeri inizino con 0
+      if (display.textContent == '0') {
+        display.textContent = digit;
+      }
+      else {
+      display.textContent += digit;
+      }
+    }
+  });
+});
